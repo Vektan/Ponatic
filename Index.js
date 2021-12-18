8aa1813e98c32f96b384b110c90c244a4ce67be3
“[ <[dylanmalware@gmail.com]>
2021-12-18 15:13:32 -0500

    Updated Index

--- Index.js
+++ Index.js
@@ -1,14 +1,28 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('fs');
 
 // Create a new client instance
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+
 // When the client is ready, run this code (only once)
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
