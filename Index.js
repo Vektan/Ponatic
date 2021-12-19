6b59d7260f54c9768c7c2f327cad721a859af438
“[ <[dylanmalware@gmail.com]>
2021-12-18 21:03:06 -0500

    Fixed Index.js and deploy-commands.js, new loaders basically.

--- Index.js
+++ Index.js
@@ -1,27 +1,20 @@
-// Require the necessary discord.js classes
+const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const fs = require('fs');
-
-// Create a new client instance
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-
-// When the client is ready, run this code (only once)
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -34,7 +27,7 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
